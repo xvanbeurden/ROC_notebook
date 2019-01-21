143eb0514ee40168dfc8e5ecffd7ac40630f8640
guidoAI <guido.de.croon@gmail.com>
2019-01-21 21:50:00 +0100

    Started the notebook

---
+++ ROC.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 12:40:14 2018
+
+Generating an ROC curve.
+
+@author: LocalAdmin
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_ROC_curve(values, classes):
+
+    # get number of positives and negatives:    
+    n_values = len(values);
+    totalP = len(np.where(classes > 0)[0]);
+    totalN = n_values - totalP;
+    
+    # sort all values:
+    inds = np.argsort(values);
+    s_values = values[inds];
+    s_classes = classes[inds];
+
+    TP = np.zeros([n_values, 1]);
+    FP = np.zeros([n_values, 1]);
+
+    for e in range(n_values):
+        # threshold = s_values[e]
+        # Positive when bigger:
+        P = np.sum(s_classes[e:]);
+        TP[e] = P / totalP;
+        
+        # number of false positives is the remaining samples above the
+        # threshold divided by all negative samples:
+        FP[e] = (len(s_classes[e:]) - P) / totalN;
+
+    return TP, FP;
+
+
+def ROC_exercise(im_name, segmentation_name, show_images = True):
+    
+    # read RGB image:
+    Im = cv2.imread(im_name);
+    WIDTH = Im.shape[1];
+    HEIGHT = Im.shape[0];
+    if(show_images):
+        cv2.imshow('Image', Im);
+        cv2.waitKey();
+    
+    # read Classification image:
+    Cl = cv2.imread(segmentation_name);
+    if(show_images):
+        cv2.imshow('Segmentation', Cl);
+        cv2.waitKey();
+    Cl = cv2.cvtColor(Cl, cv2.COLOR_BGR2GRAY);
+    Cl = Cl.flatten();
+    Cl = Cl > 0;
+    Cl = Cl.astype(float);
+    
+    # make a meshgrid:
+    x, y = np.meshgrid(range(WIDTH), range(HEIGHT));
+    x = x.flatten();
+    y = y.flatten();
+    
+    # take the blue channel, cv2 represents images as BGR:
+    # TODO: play with the next expression:
+    Values = -y #Im[:,:,0];#2];
+    Values = Values.flatten();
+
+    
+    # get the ROC curve:
+    TP, FP = get_ROC_curve(Values, Cl);
+
+    plt.figure();
+    plt.plot(FP, TP, 'b');
+    plt.xlabel('TP');
+    plt.ylabel('FP');
+
+if __name__ == '__main__':
+    im_name = 'CroppedImage.bmp';
+    segmentation_name = 'TreeSegmentation.bmp';
+    ROC_exercise(im_name, segmentation_name);
